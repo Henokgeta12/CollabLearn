75bdcd189cbc65d4d1cd166236f23fadf1692b24
henokassfaw <henokasfaw@gmail.com>
2024-08-30 19:43:05 +0300

    add create_group and group_details routes modify study group table

--- app/__init__.py
+++ app/__init__.py
@@ -22,6 +22,7 @@ def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(Config)
 
+
     # Initialize extensions
     db.init_app(app)
     login_manager.init_app(app)
