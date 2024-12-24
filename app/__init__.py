3c8e0c4fcbc23420c55ee32d234e96445d01b393
Henokgeta12 <henokasfaw47@icloud.com>
2024-12-24 20:29:51 +0300

    Committing local changes before pulling

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,7 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from .extensions import db, login_manager, migrate
+from app.extensions import db, login_manager, migrate
 from .models.user_models import Users
-from CollabLearn.config import Config  
-
+from config import Config
 
 
 def create_app(config_class=Config):
@@ -18,7 +16,7 @@ def create_app(config_class=Config):
         Flask: The created Flask application instance.
     """
     app = Flask(__name__, template_folder="templates")
-    app.config.from_object(Config)
+    app.config.from_object(config_class)
 
     # Initialize extensions
     db.init_app(app)
@@ -32,7 +30,7 @@ def create_app(config_class=Config):
 
     # Register routes
     with app.app_context():
-        from .routes import register_routes  # Import and register routes
+        from app.routes import register_routes  # Import and register routes
         register_routes(app)
 
     return app
