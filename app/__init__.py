07ef60f06a567df38f6656e02a19fe1afe553531
Henokgeta12 <henokasfaw47@icloud.com>
2025-01-23 23:02:19 +0300

    add doc string my my init file

--- app/__init__.py
+++ app/__init__.py
@@ -2,33 +2,42 @@ from flask import Flask
 from .extensions import db, login_manager, migrate
 from config import Config
 from flask_wtf.csrf import CSRFProtect
+import os
+from dotenv import load_dotenv
 
 
+    def create_app(config_class=Config):
+        """
+        Create and configure the Flask application.
 
-def create_app(config_class=Config):
-    """
-    Creates a Flask application instance with the specified configuration.
+        This function sets up the Flask application instance using the provided
+        configuration class. It initializes various extensions, configures
+        application settings, and registers routes.
 
-    Args:
-        config_class (Config): The configuration class to use for the application.
-            Defaults to Config.
+        Args:
+            config_class (class): The configuration class to use for setting
+            application configuration variables.
 
-    Returns:
-        Flask: The created Flask application instance.
-    """
+        Returns:
+            app (Flask): The configured Flask application instance.
+        """
+
     app = Flask(__name__, template_folder="templates")
     app.config.from_object(config_class)
     Config.init_app(app)
     app.config['UPLOAD_FOLDER'] = 'static/user_profile-pic'
-    # app.config['MAX_CONTENT_LENGTH'] = 2 * 1024 * 1024  # Limit upload size to 2MB
-    CSRFProtect(app)
-
+    app.config['STATIC_FOLDER'] = '/static'
+    app.config['STATIC_URL_PATH'] = '/static'
+    
 
     # Initialize extensions
     db.init_app(app)
     login_manager.init_app(app)
     login_manager.login_view = 'login'
     migrate.init_app(app, db)
+
+    # Initialize CSRF protection
+    csrf = CSRFProtect(app)
 
     @login_manager.user_loader
     def load_user(user_id):
@@ -37,7 +46,8 @@ def create_app(config_class=Config):
 
     # Register routes
     with app.app_context():
-        from app.routes import register_routes  # Import and register routes
+        from app.routes import register_routes
         register_routes(app)
 
     return app
+
